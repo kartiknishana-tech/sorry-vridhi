4ac0b47888f3a65cc6d61cc9a9c550770fc78b93
kartiknishana-tech <kartiknishana@gmail.com>
2025-12-10 16:51:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// This is optional. You can use JavaScript for additional functionality if needed.
+document.querySelector(".next-btn").addEventListener("click", function () {
+    alert("Moving on to the next page! ❤️");
+});
